f17da1b27d7f3f3d4f0621acb0d6cfbfb63a12f5
Shane <shanemcneil19@gmail.com>
2019-09-30 09:24:28 -0700

    add NotePage component

--- src/NotePage/NotePage.js
+++ src/NotePage/NotePage.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import Notes from './Notes/Notes';
+import './NotePage.css';
 
-export default function NotePage() {
-    return null;
+export default function NotePage(props) {
+    return (
+        <section className='NotePage'>
+            <Notes
+                id={props.note.id}
+                name={props.note.name}
+                modified={props.note.modified}
+            />
+            <div className='NotePage-content'>
+                {props.note.content.split(/\n \r|\n/).map((para, i) =>
+                    <p key={i}>{para}</p>
+                )}
+            </div>
+        </section>
+    )
+}
+
+NotePage.defaultProps = {
+    note: {
+        content: '',
+    }
 }
