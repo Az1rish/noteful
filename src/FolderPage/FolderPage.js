5a2744543fe90fd56a5f00dace12edbf900ce23e
Shane <shanemcneil19@gmail.com>
2019-09-30 09:33:06 -0700

    add FolderPage component

--- src/FolderPage/FolderPage.js
+++ src/FolderPage/FolderPage.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import STORE from '../Store';
+import { NavLink, Link } from 'react-router-dom';
+import './FolderPage.css';
 
-export default function FolderPage() {
+export default function FolderPage(props) {
+    const countNotesForFolder = (notes=[], folderId) =>
+        notes.filter(note => note.folderId === folderId).length
     return (
-        null
+        <div className='FolderPage'>
+            <ul className='FolderPage-list'>
+                {props.folders.map(folder =>
+                    <li key={folder.id}>
+                        <NavLink
+                            className='FolderPage-folder'
+                            to={`/folder/${folder.id}`}
+                        >
+                            <span className='FolderPage-num'>
+                                {countNotesForFolder(props.notes, folder.id)}
+                            </span>
+                            {folder.name}
+                        </NavLink>
+                    </li>
+                )}
+            </ul>
+            <div className='FolderPage-button'>
+                <button>Add Folder</button>
+            </div>
+        </div>
     )
+}
+
+FolderPage.defaultProps = {
+    folders: []
 }
