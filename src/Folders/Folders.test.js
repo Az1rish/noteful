a6341196fdb3c4186092768173ed82a269d97d3d
Shane <shanemcneil19@gmail.com>
2019-11-23 07:07:13 -0700

    add Folders test, passing

--- src/Folders/Folders.test.js
+++ src/Folders/Folders.test.js
@@ -4,6 +4,11 @@ import Folders from './Folders';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Folders />, div);
+    const defaultProps = {
+        match: {
+            params: { note_id: 9876 }
+        }
+    }
+    ReactDOM.render(<Folders {...defaultProps}/>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
