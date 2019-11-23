fd9a93ef69b5de390016a44bd3ed7852fbb42782
Shane <shanemcneil19@gmail.com>
2019-11-23 07:10:17 -0700

    add FolderPage test, passing

--- src/FolderPage/FolderPage.test.js
+++ src/FolderPage/FolderPage.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FolderPage from './FolderPage';
+import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<FolderPage />, div);
+    ReactDOM.render(
+    <BrowserRouter>
+        <FolderPage />
+    </BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
